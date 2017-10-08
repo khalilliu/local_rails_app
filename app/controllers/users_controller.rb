05686e5c143d81b14597fde75df78e09d39697b7
khalilliu <lkhaliliu@gmail.com>
2017-10-08 17:53:08 +0800

    add articles to user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,8 +31,13 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def show
+		@user = User.find(params[:id])
+	end
+
 	private
 		def user_params
 			params.require(:user).permit(:username,:email,:password)
 		end
+		
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	def gravatar_for(user, options={size:80})
+		
+		size = options[:size]
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)		
+		gravatar_url = "https://www.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+		
+		example = "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
+		image_tag(gravatar_url, alt:user.username, class:'img-circle')
+	end
 end
