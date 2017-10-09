8a9623b877d2a31900725db5b3151b882b2d5686
khalilliu <lkhaliliu@gmail.com>
2017-10-09 23:40:18 +0800

    add admin to delete users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
 	before_action :set_user, only: [:edit, :update, :show]
-	before_action :require_same_user, only: [:edit,:update]
+	before_action :require_same_user, only: [:edit, :update, :destroy]
+	before_action :require_admin, only: [:destroy]
 
 	def index
 		@users = User.paginate(page: params[:page], per_page:3)
@@ -29,9 +30,7 @@ class UsersController < ApplicationController
 	def update
 		
 		
-		#params[:user][:password ] = @user.password if params[:user][:password].blank?
-
-		
+		#params[:user][:password ] = @user.password if params[:user][:password].blank?	
 		if @user.update(user_params)
 			flash[:success]='Your account was updated successfully'
 			redirect_to articles_path
@@ -45,6 +44,13 @@ class UsersController < ApplicationController
 		@user_articles = @user.articles.paginate(page:params[:page], per_page:3)
 	end
 
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		flash[:danger] = "User and all articles created by user have been deleted"
+		redirect_to users_path
+	end
+
 	private
 		def user_params
 			params.require(:user).permit(:username,:email,:password)
@@ -55,9 +61,16 @@ class UsersController < ApplicationController
 		end
 
 		def require_same_user
-			if current_user != @user
+			if current_user != @user and !current_user.admin
 				flash[:danger] = 'You can only edit your own account'
 				redirect_to root_path
 			end
 		end 
+
+		def require_admin
+			if logged_in? and !current_user.admin?
+				flash[:danger] = 'only admin user can peform that action'
+				redirect_to users_path
+			end
+		end
 end
