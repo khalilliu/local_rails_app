7ad30c63764b665828789f9bc4f4280964946623
khalilliu <lkhaliliu@gmail.com>
2017-10-09 22:53:30 +0800

    add signin upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,9 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			session[:user_id] = @user.id
 			flash[:success] = 'welcome to the alpha blog'+@user.username
-			redirect_to articles_path
+			redirect_to user_path(@user)
 		else
 			render 'new'
 		end
@@ -58,5 +59,5 @@ class UsersController < ApplicationController
 				flash[:danger] = 'You can only edit your own account'
 				redirect_to root_path
 			end
-		end
+		end 
 end
